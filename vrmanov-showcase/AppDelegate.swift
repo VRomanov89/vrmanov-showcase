11daceb1195621db7ae5a428ab099591078bbd55
Vladimir Romanov <vladromweb@gmail.com>
2015-12-21 17:12:11 -0500

    Facebook Login Implementation
    Facebook Login Implementation

--- vrmanov-showcase/AppDelegate.swift
+++ vrmanov-showcase/AppDelegate.swift
@@ -37,7 +37,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationDidBecomeActive(application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        FBSDKAppEvents.activateApp()
+    }
+    
+    func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?, annotation: AnyObject) -> Bool {
+        return FBSDKApplicationDelegate.sharedInstance().application(application, openURL: url, sourceApplication: sourceApplication, annotation: annotation)
     }
 
     func applicationWillTerminate(application: UIApplication) {
