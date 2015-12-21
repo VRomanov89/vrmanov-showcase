01ee23bef0a5783a48ffeb0715740a897f9e04a5
Vladimir Romanov <vladromweb@gmail.com>
2015-12-21 14:41:14 -0500

    Added Shadows
    Added Shadows

--- vrmanov-showcase/MaterialButton.swift
+++ vrmanov-showcase/MaterialButton.swift
@@ -1,5 +1,5 @@
 //
-//  MaterialView.swift
+//  MaterialButton.swift
 //  vrmanov-showcase
 //
 //  Created by Volodymyr Romanov on 12/21/15.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class MaterialView: UIView {
+class MaterialButton: UIButton {
 
     override func awakeFromNib() {
         layer.cornerRadius = 2.0
