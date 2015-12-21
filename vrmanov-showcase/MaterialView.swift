561e8eaaef858633f01527091dbfb971289115a9
Vladimir Romanov <vladromweb@gmail.com>
2015-12-21 14:07:33 -0500

    Initial Commit
    Initial Commit

---
+++ vrmanov-showcase/MaterialView.swift
@@ -0,0 +1,21 @@
+//
+//  MaterialView.swift
+//  vrmanov-showcase
+//
+//  Created by Volodymyr Romanov on 12/21/15.
+//  Copyright © 2015 EEEnthusiast. All rights reserved.
+//
+
+import UIKit
+
+class MaterialView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
