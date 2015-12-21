2df2474f96a565457bf5d1c32e7916bea4ab3967
Vladimir Romanov <vladromweb@gmail.com>
2015-12-21 16:29:56 -0500

    Added Firebase to Project through Cocoapods
    Added Firebase to Project through Cocoapods

---
+++ Pods/Target Support Files/Pods-vrmanov-showcase/Pods-vrmanov-showcase-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_vrmanov_showcase : NSObject
+@end
+@implementation PodsDummy_Pods_vrmanov_showcase
+@end
---
+++ Pods/Target Support Files/Pods-vrmanov-showcase/Pods-vrmanov-showcase-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_vrmanov_showcaseVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_vrmanov_showcaseVersionString[];
+
